70a455625fdeee8e77a78758e699c607c918a30b
Alex Sexton <alexsexton@gmail.com>
2016-02-01 04:25:12 -0600

    Start modifying stuff. Make sure we can add in the image gallery.

--- src/components/ContentPage/ContentPage.js
+++ src/components/ContentPage/ContentPage.js
@@ -9,7 +9,23 @@
 
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import ImageGallery from 'react-image-gallery';
 import s from './ContentPage.scss';
+
+var images = [
+  {
+    original: '/img/cocktails/10yearhunter.jpg',
+    thumbnail: '/img/cocktails/10yearhunter.jpg',
+  },
+  {
+    original: '/img/cocktails/coneyislandfakir.jpg',
+    thumbnail: '/img/cocktails/coneyislandfakir.jpg',
+  },
+  {
+    original: '/img/cocktails/fixedflight.jpg',
+    thumbnail: '/img/cocktails/fixedflight.jpg',
+  }
+];
 
 class ContentPage extends Component {
 
@@ -30,6 +46,11 @@ class ContentPage extends Component {
         <div className={s.container}>
           {this.props.path === '/' ? null : <h1>{this.props.title}</h1>}
           <div dangerouslySetInnerHTML={{ __html: this.props.content || '' }} />
+          <ImageGallery
+            items={images}
+            autoPlay={true}
+            server={true}
+            slideInterval={8000} />
         </div>
       </div>
     );
